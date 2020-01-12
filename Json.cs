f3415dd3867f9a5e123a939737a4b6031b096704
ferizoozoo <farhadzohoorz@gmail.com>
2020-01-12 14:04:40 +0330

    Writing the write to a json file function

--- Json.cs
+++ Json.cs
@@ -6,12 +6,37 @@ namespace JsonX
 {
     public class Json
     {
-        private Dictionary<string,string> JsonDictionary { get; set; }
+        public Dictionary<string,string> JsonDictionary { get; private set; }  = new Dictionary<string, string>();
 
-        // TODO : Complete the function below!
         // Writes the JsonDictionary into a .json file.
         private void WriteJsonFile(string toFilePathName)
         {
+            int jsonDictionaryCount = this.JsonDictionary.Count;
+
+            if (jsonDictionaryCount == 0)
+                throw new Exception("The JsonDictionary is empty.");
+
+            using (var file = new StreamWriter(toFilePathName))
+            {
+                // Starting curve bracket of the new json file.
+                file.WriteLine("{");
+
+                foreach (var dictionaryElement in JsonDictionary)
+                {
+                    string jsonElement = $"\t\"{dictionaryElement.Key}\" : \"{dictionaryElement.Value}\"";
+
+                    // Checking if there is another field for adding a comma or not.
+                    if (jsonDictionaryCount > 1)
+                        jsonElement += ",";
+
+                    file.WriteLine(jsonElement);   
+
+                    jsonDictionaryCount--; 
+                }    
+
+                // Ending curve bracket of the new json file.
+                file.WriteLine("}");
+            }
         }
 
         // Reads a text file and turns it into a dictionary of Json pieces.
@@ -19,23 +44,28 @@ namespace JsonX
         {
             if (File.Exists(fromFilePathName))
             {
-                using (var file = File.OpenText(fromFilePathName))
+                using (var file = new StreamReader(fromFilePathName))
                 {
-                    string[] jsonComponents = file.ReadToEnd().Split(',');
+                    string[] jsonComponents = file.ReadToEnd().Trim().Split(',');
+
                     foreach (var component in jsonComponents)
                     {
-                        string[] jsonComponent = component.Split(':');
-                        this.JsonDictionary[jsonComponent[0]] = jsonComponent[1];
+                        string[] jsonComponent = component.Trim().Split(':');
+                        this.JsonDictionary.Add(jsonComponent[0], jsonComponent[1]);
                     }
                 }
+            }
+            else
+            {
+                throw new Exception("Couldn't find the file path.");
             }
         }
 
         // Does the whole process of turning a text file into a .json file
-        public void GetThatFuckingJson(string fromFilePathName, string toFilePathName)
+        public void GetThatFuckingJsonFile(string filePathName)
         {
-            ReadFileToJsonDictionary(fromFilePathName);
-            WriteJsonFile(toFilePathName);
+            this.ReadFileToJsonDictionary(filePathName);
+            this.WriteJsonFile($"{Path.GetFileNameWithoutExtension(filePathName)}.json");
         }
     }
 }
